93dc286547c3b5556de82fa9b9cbcbce0e6115dd
Minh Tri <ngoc-minh-tri.truong@imt-atlantique.net>
2020-04-11 16:00:57 +0200

    Added pyrat game files

---
+++ pyrat_env/imports/dummyplayer.py
@@ -0,0 +1,7 @@
+TEAM_NAME = "Dummy"
+def preprocessing(mazeMap, mazeWidth, mazeHeight, playerLocation, opponentLocation, piecesOfCheese, timeAllowed):
+    return
+def turn (mazeMap, mazeWidth, mazeHeight, playerLocation, opponentLocation, playerScore, opponentScore, piecesOfCheese, timeAllowed):
+    return
+def postprocessing (mazeMap, mazeWidth, mazeHeight, playerLocation, opponentLocation, playerScore, opponentScore, piecesOfCheese, timeAllowed):
+    return
