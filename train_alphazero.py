0c60414c2323aaa00f08a0caa6558bf4ba28463e
Minh Tri <ngoc-minh-tri.truong@imt-atlantique.net>
2020-04-26 17:15:13 +0200

    More hyperparameter tweaking

--- train_alphazero.py
+++ train_alphazero.py
@@ -11,9 +11,9 @@ args = dotdict({
     'tempThreshold': 40,        #
     'updateThreshold': 0.5790,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 1000000,    # Number of game examples to train the neural networks.
-    'numMCTSSims': 600,          # Number of games moves for MCTS to simulate.
+    'numMCTSSims': 40,          # Number of games moves for MCTS to simulate.
     'arenaCompare': 40,         # Number of games to play during arena play to determine if new net will be accepted.
-    'cpuct': 1,
+    'cpuct': 4,
 
     'checkpoint': './temp/3x64/',
     'load_model': False,
