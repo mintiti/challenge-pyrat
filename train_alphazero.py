85a6cfc54edcb594a588c2b192c1deae695d54a8
Minh Tri <ngoc-minh-tri.truong@imt-atlantique.net>
2020-05-04 23:29:09 +0200

    hyperparameter tuning
    More mcts sims now that i know it works

--- train_alphazero.py
+++ train_alphazero.py
@@ -13,7 +13,7 @@ args = dotdict({
     'updateThreshold': 0.5790,
     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
     'maxlenOfQueue': 50000,  # Number of game examples to train the neural networks.
-    'numMCTSSims': 200,  # Number of games moves for MCTS to simulate.
+    'numMCTSSims': 600,  # Number of games moves for MCTS to simulate.
     'arenaCompare': 20,  # Number of games to play during arena play to determine if new net will be accepted.
     'cpuct': 2,
 
