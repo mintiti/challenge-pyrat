bf29b718d6a6f6121eb9c5ba8b65edea7010c147
iPown <minhtri3007@hotmail.com>
2020-05-24 19:34:25 +0200

    Script to add games from an external source, such as google colab

---
+++ add_games_buffer.py
@@ -0,0 +1,8 @@
+from train_parallel_alphazero import args
+from agents.alphazero.parallel.buffer import ReplayBuffer
+
+if __name__ == '__main__':
+    buffer = ReplayBuffer(args['checkpoint'] + 'examples/', maxlen=args['buffer_size'])
+    buffer.load()
+    print(len(buffer))
+    print(buffer.storage.maxlen)
